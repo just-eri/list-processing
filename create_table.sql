0cebed8fe5b527879fb0ca52bc112355f2888748
vegtamr <barekov.am.v@gmail.com>
2024-10-04 17:33:34 +0500

    add table creating script

---
+++ create_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS entries (
+  name VARCHAR(255),
+  posX REAL,
+  posY REAL,
+  type VARCHAR(255),
+  time REAL
+);
+
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Кривой', -37.23, 13.44, 'Человек', 1693235249);
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Магазин', 0.163, 2.119, 'Здание', 1693135218);
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Лада', 118.3, -982.041, 'Машина', 1692748121);
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Челюскин', -0.1, 0.33, 'Человек', 1727767637);
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Шарик', 323.3, 172.16, 'Собака', 1728026837);
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Автобус', 90.1, -98233.041, 'Машина', 1725434837);
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Двойной автобус', 180.2, -196466.082, 'Машина', 1725391637);
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Нулевой автобус', 0, 0, 'Машина', 1599161237);
+INSERT INTO entries (name, posX, posY, type, time) VALUES ('Креветка', 17.3, 9.2, 'Креветка', 1727897237);
